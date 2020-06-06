197a85da3eb4159b48076df1e0faa53c37eae6f3
Alexander Sholk <alexsholk@yandex.by>
2020-06-06 20:57:16 +0300

    Angular integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { StoreComponent } from './store/store.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    StoreComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,15 @@
+import {Component, ViewEncapsulation} from '@angular/core'
+
+@Component({
+  selector: '.main-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class FooterComponent {
+  links = [
+    {route: ['/p', 'payment-details'], title: 'Реквизиты'},
+    {route: ['/p', 'ua'], title: 'Пользовательское соглашение'},
+    {route: ['/p', 'pdpa'], title: 'Соглашение на обработку персональных данных'},
+  ]
+}
