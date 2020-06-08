dc2f39fbd0fd9814c1c9f931ee73b99605bbb763
Alexander Sholk <alexsholk@yandex.by>
2020-06-08 21:48:16 +0300

    choose btn

--- src/app/store/product/product.component.ts
+++ src/app/store/product/product.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core'
+import {Component, HostBinding, Input, OnInit, Renderer2, ViewEncapsulation} from '@angular/core'
 import {ProductWrapper} from '../types'
 
 @Component({
@@ -9,11 +9,22 @@ import {ProductWrapper} from '../types'
 })
 export class ProductComponent implements OnInit {
   @Input() product: ProductWrapper
+  @HostBinding('class.chosen') isChosen: boolean
 
-  constructor() {
+  constructor(private renderer: Renderer2) {
   }
 
   ngOnInit(): void {
+
   }
 
+  choose() {
+    this.isChosen = true
+    this.renderer.addClass(document.body, 'modal-open')
+  }
+
+  unChoose() {
+    this.isChosen = false
+    this.renderer.removeClass(document.body, 'modal-open')
+  }
 }
--- src/app/store/types.ts
+++ src/app/store/types.ts
@@ -1,3 +1,5 @@
+import {BehaviorSubject} from 'rxjs'
+
 export interface Category {
   id: number
   parent_id?: number
@@ -7,6 +9,24 @@ export interface Category {
   }
 }
 
+export interface Option {
+  code: string
+  title: string
+  type: OptionType
+  required: boolean
+  values: OptionValue[]
+  value?: number // synthetic
+}
+
+export enum OptionType {
+  select
+}
+
+export interface OptionValue {
+  title: string
+  options: Option[]
+}
+
 export interface Product {
   id: number
   category_id: number
@@ -14,13 +34,28 @@ export interface Product {
     title: string
     description: string
     image: string
+    options: Option[]
   }
 }
 
 export class ProductWrapper {
+  public options$: BehaviorSubject<Option[]> = new BehaviorSubject<Option[]>([])
 
   constructor(
     private product: Product) {
+    this.options$.next([...this.iterateOptions(product.data.options)])
+  }
+
+  private* iterateOptions(options: Option[]) {
+    for (const option of options) {
+      yield option
+      if (option.value === undefined) {
+        option.value = 0
+      }
+      if (option.values.length && option.values[option.value]?.options) {
+        yield* this.iterateOptions(option.values[option.value].options)
+      }
+    }
   }
 
   getTitle(): string {
@@ -34,5 +69,14 @@ export class ProductWrapper {
   getImageSrc(): string {
     // todo replace
     return '../../../assets/mock/' + this.product.data.image
+  }
+
+  setOptionValue(optionCode: string, value: number) {
+    for (const option of this.iterateOptions(this.product.data.options)) {
+      if (option.code === optionCode && option.values[value]) {
+        option.value = value
+      }
+    }
+    this.options$.next([...this.iterateOptions(this.product.data.options)])
   }
 }
