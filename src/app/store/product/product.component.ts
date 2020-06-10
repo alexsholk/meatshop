dec017fdb4e3137d337730d84a9dad16d718ae92
Alexander Sholk <alexsholk@yandex.by>
2020-06-10 14:59:35 +0300

    add to cart close

--- src/app/store/product/product.component.ts
+++ src/app/store/product/product.component.ts
@@ -1,4 +1,4 @@
-import {Component, HostBinding, HostListener, Input, OnInit, Renderer2, ViewEncapsulation} from '@angular/core'
+import {Component, HostBinding, HostListener, Input, Renderer2, ViewEncapsulation} from '@angular/core'
 import {Option, ProductWrapper} from '../types'
 import {CartService} from '../cart/cart.service'
 
@@ -8,42 +8,43 @@ import {CartService} from '../cart/cart.service'
   styleUrls: ['./product.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent {
   @Input() product: ProductWrapper
-  @HostBinding('class.active') isChosen: boolean
+  @HostBinding('class.active') isActive: boolean
   public isOptionsOpen = false
   public activeOption: Option = null
 
   constructor(
     private renderer: Renderer2,
-    public cart: CartService) {
+    private cart: CartService) {
   }
 
   @HostListener('window:resize', ['$event.target'])
   onWindowResize(event) {
     if (event.innerWidth >= 600) {
-      this.unChoose()
+      this.deactivate()
     } else {
       this.isOptionsOpen = false
     }
   }
 
-  ngOnInit(): void {
-
-  }
-
-  choose() {
-    this.isChosen = true
+  activate() {
+    this.isActive = true
     this.renderer.addClass(document.body, 'modal-open')
   }
 
-  unChoose() {
-    this.isChosen = false
+  deactivate() {
+    this.isActive = false
     this.renderer.removeClass(document.body, 'modal-open')
   }
 
   openOptions(option) {
     this.activeOption = option
     this.isOptionsOpen = true
+  }
+
+  addToCart(product: ProductWrapper) {
+    this.cart.addItem(product)
+    this.deactivate()
   }
 }
