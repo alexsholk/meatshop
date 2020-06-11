67d174d8d1cd55dffc30ae238ac6cab073dbed40
Alexander Sholk <alexsholk@yandex.by>
2020-06-11 14:55:50 +0300

    quantity timer

--- src/app/store/product/product.component.ts
+++ src/app/store/product/product.component.ts
@@ -1,6 +1,7 @@
-import {Component, EventEmitter, Input, Output, ViewEncapsulation} from '@angular/core'
+import {Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation} from '@angular/core'
 import {Option, OptionValue, ProductWrapper} from '../types'
 import {CartService} from '../cart/cart.service'
+import {interval, Observable, Subscription} from 'rxjs'
 
 @Component({
   selector: '[app-product]',
@@ -8,7 +9,7 @@ import {CartService} from '../cart/cart.service'
   styleUrls: ['./product.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
 
   constructor(
     private cart: CartService) {
@@ -16,11 +17,17 @@ export class ProductComponent {
 
   @Input() product: ProductWrapper
   @Output() activateProduct: EventEmitter<number> = new EventEmitter<number>()
+  private timer$: Observable<number>
+  private timerSub: Subscription
 
   public activeOption: Option = null
 
   private static isSmallDisplay(): boolean {
     return window.innerWidth < 600
+  }
+
+  ngOnInit(): void {
+    this.timer$ = interval(160)
   }
 
   activate() {
@@ -53,5 +60,19 @@ export class ProductComponent {
     if (option.value !== undefined && option.value !== null) {
       return option.values[option.value]
     }
+  }
+
+  decreaseTimerOn() {
+    this.timerOff()
+    this.timerSub = this.timer$.subscribe(() => this.product.decreaseQuantity())
+  }
+
+  timerOff() {
+    this.timerSub?.unsubscribe()
+  }
+
+  increaseTimerOn() {
+    this.timerOff()
+    this.timerSub = this.timer$.subscribe(() => this.product.increaseQuantity())
   }
 }
